0e216a2bd518a2a5f6fe99c6c43f6fba4df4efe5
himanshi11 <himanshigarg1102@gmail.com>
2026-05-26 23:18:51 +0530

    fix

--- src/components/PremiumChat.jsx
+++ src/components/PremiumChat.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 const PremiumChat = () => {
+    const {getUserId}=useParams()
+    console.log(useParams)
   return (
     <div>PremiumChat</div>
   )
